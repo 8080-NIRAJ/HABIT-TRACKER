40f308080bbd76470504e810196f1e6a54bd0be8
8080-NIRAJ <71283784+8080-NIRAJ@users.noreply.github.com>
2024-01-08 20:31:16 +0530

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -1,14 +1,23 @@
-import mongoose from "mongoose";
 
-const url ='mongodb://localhost:27017/habitTrackerDB';
+import mongoose from 'mongoose';
+
+const password = 'Niraj8999';
+
+// Create the MongoDB connection URL with the encoded password
+
+const dbURI = `mongodb+srv://Niraj80:${password}@cluster0.mzowurg.mongodb.net/`;
+// Connect to the MongoDB server with the provided URL and options (none specified in this case)
+
+
+
 export const connectUsingMongoose =async()=>{
     try{
-        await mongoose.connect(url,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-       
-
+      mongoose.connect(dbURI, {
+        // Options can be added here if needed
+        tls: true,
+        tlsAllowInvalidCertificates: true,
+      });
+      console.log("Mongodb connected successfully!!!")
     }catch(err){
        console.log(err);
     }
